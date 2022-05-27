e5a766873a831f3242cef5eeca6042eccf52d60f
Monzyk, Erik (em7272) <erikmonzyk@att.com>
2022-05-26 20:31:03 -0500

    refactored the notification function.

--- function/ServiceBusQFunction/__init__.py
+++ function/ServiceBusQFunction/__init__.py
@@ -14,10 +14,8 @@ msg: func.ServiceBusMessage
 
 def main(msg: func.ServiceBusMessage):
 
-    notification_id = (msg.get_body().decode('utf-8'))
-    logging.info(notification_id)
-    
-    #notification_id = int(msg.get_body().decode('utf-8'))
+    #notification_id = (msg.get_body().decode('utf-8'))
+    notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
 
     # TODO: Get connection to database
@@ -27,22 +25,40 @@ def main(msg: func.ServiceBusMessage):
     
     try:
     
-        get_msg_subject = cur.execute("SELECT subject, message FROM notification WHERE id = {};".format(notification_id,))
-
+        cur.execute("SELECT subject, message FROM notification WHERE id = {};".format(notification_id,))
+        rows = cur.fetchall()
+        rows = rows [0]
+        subject = str(rows[0])
+        body = str(rows[1])
+        
         # TODO: Get attendees email and name
         logging.info('Fetching attendees email and name...')
         cur.execute("SELECT email, first_name FROM attendee;")
         attendees = cur.fetchall()
 
         # # Loop through attendees
-        for attendee in attendees:
-            Mail('{}, {}, {}'.format({'john@doe.com'}, {attendee[2]}, {get_msg_subject}))
-    
-        completed_date = datetime.utcnow()
+        # for attendee in attendees:
+        #     Mail('{}, {}, {}'.format({'john@doe.com'}, {attendee[2]}, {rows}))
+
+        #Try different loop 
+        for (email, first_name) in attendees:
+            mail = Mail(
+                from_email='erikmonzyk@techconf.com',
+                to_emails= email,
+                subject= subject,
+                plain_text_content= "Hi {}, \n {}".format(first_name, body))
+            try:
+                SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
+                send_grid = SendGridAPIClient(SENDGRID_API_KEY)
+                response = send_grid.send(mail)
+            except Exception as e:
+                logging.error(e)
+                
+        #completed_date = datetime.utcnow()
         
         notify_attendees = "Notified {} attendees".format(len(attendees))
         
-        update_notifications = cur.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notify_attendees, completed_date, notification_id))
+        cur.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notify_attendees, datetime.utcnow(), notification_id))
         db.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
@@ -53,15 +69,4 @@ def main(msg: func.ServiceBusMessage):
         cur.close()
         db.close()
         
-#SendGrid Email brought over from routes.py        
-        
-def send_email(email, subject, body):
-    if not app.config.get('SENDGRID_API_KEY'):
-        message = Mail(
-            from_email=app.config.get('ADMIN_EMAIL_ADDRESS'),
-            to_emails=email,
-            subject=subject,
-            plain_text_content=body)
 
-        sg = SendGridAPIClient(app.config.get('SENDGRID_API_KEY'))
-        sg.send(message)
