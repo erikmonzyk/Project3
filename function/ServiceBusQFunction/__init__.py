548251af3bd1de00864ac3894c4c6b1795eaded8
Monzyk, Erik (em7272) <erikmonzyk@att.com>
2022-06-02 14:32:49 -0500

    updated notification_id to int form string.

--- function/ServiceBusQFunction/__init__.py
+++ function/ServiceBusQFunction/__init__.py
@@ -12,7 +12,7 @@ def main(msg: func.ServiceBusMessage):
     #notification_id = (msg.get_body().decode('utf-8'))
     #print('notification_id: {} enqueued to queue: {}'.format(msg.get_body().decode('utf-8')))
     
-    notification_id = str(msg.get_body().decode('utf-8'))
+    notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
 
     # TODO: Get connection to database
@@ -23,7 +23,8 @@ def main(msg: func.ServiceBusMessage):
     
     try:
     
-        query = cur.execute("SELECT subject, message FROM notification WHERE id = {};".format(notification_id))
+        query = cur.execute("SELECT message, subject FROM notification WHERE id = {};".format(notification_id))
+
         rows = cur.fetchall()
         rows = rows [0]
         subject = str(rows[0])
