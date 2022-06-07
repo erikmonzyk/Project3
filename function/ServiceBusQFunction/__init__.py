8f7a6c2d0150a2a4be84c91de8d50cea6910701d
Monzyk, Erik (em7272) <erikmonzyk@att.com>
2022-06-07 18:20:51 -0500

    reverted to a past commit.

--- function/ServiceBusQFunction/__init__.py
+++ function/ServiceBusQFunction/__init__.py
@@ -2,51 +2,69 @@ import logging
 import azure.functions as func
 import psycopg2
 import os
+#from web.app.models import Attendee, Conference, Notification
 from datetime import datetime
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
 
-        notification_id =str(msg.get_body().decode('utf-8'))
-        logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
-        
-
-        # TODO: Get connection to database
-
-        connection = psycopg2.connect(host="project3server.postgres.database.azure.com", dbname="techconfdb", user="erikmonzyk@project3server", password="Carson2013$$")
-        
-        cur = connection.cursor()
-        
-        try:
+    #notification_id = (msg.get_body().decode('utf-8'))
+    #print('notification_id: {} enqueued to queue: {}'.format(msg.get_body().decode('utf-8')))
     
-            # Get notification message and subject from database using the notification_id
-            logging.info('Fetching notification message and subject...')
-            notification_query = cur.execute("SELECT message, subject FROM notification WHERE id = {};".format(notification_id))
+    notification_id = str(msg.get_body().decode('utf-8'))
+    logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
 
-            logging.info('Fetching attendees email and name...')
-            cur.execute("SELECT email, first_name FROM attendee;")
-            attendees = cur.fetchall()
-            
-            # Loop through each attendee and send an email with a personalized subject
-            logging.info('Sending emails...')
-            for attendee in attendees:
-                Mail('{}, {}, {}'.format({'john@doe.com'}, {attendee[2]}, {notification_query}))
-            
-                #Update notification table by setting completed_date
-                completed_date = datetime.utcnow()
-                #total_attendees = len(attendees)
-                logging.info('Updating notifications...')
-                notification_status = 'Notified {} attendees'.format(len(attendees))
-                update_query = cur.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notification_status, completed_date, notification_id))
-                connection.commit()
+    # TODO: Get connection to database
 
-        except (Exception, psycopg2.DatabaseError) as error:
-                logging.error(error)
-                connection.rollback()
+    db = psycopg2.connect(host="project3server.postgres.database.azure.com", dbname="techconfdb", user="erikmonzyk@project3server", password="Carson2013$$")
+    cur = db.cursor()
+    
+    
+    try:
+    
+        query = cur.execute("SELECT subject, message FROM notification WHERE id = {};".format(notification_id))
+        rows = cur.fetchall()
+        rows = rows [0]
+        subject = str(rows[0])
+        body = str(rows[1])
+        
+        # TODO: Get attendees email and name
+        logging.info('Fetching attendees email and name...')
+        cur.execute("SELECT email, first_name FROM attendee;")
+        attendees = cur.fetchall()
+
+        # # Loop through attendees
+        logging.info('Sending email to attendees')
+        for attendee in attendees:
+             Mail('{}, {}, {}'.format({'xxxx@xxxx.com'}, {attendee[2]}, {query}))
+
+        #Try different loop 
+        # for (email, first_name) in attendees:
+        #     mail = Mail(
+        #         from_email='erikmonzyk@techconf.com',
+        #         to_emails= email,
+        #         subject= subject,
+        #         plain_text_content= "Hi {}, \n {}".format(first_name, body))
+           # try:
+            # SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
+            # send_grid = SendGridAPIClient(SENDGRID_API_KEY)
+            # response = send_grid.send(mail)
+            # except Exception as e:
+            #     logging.error(e)
             
-        finally:
-            cur.close()
-            connection.close()
+        new_completed_date = datetime.utcnow()
+        status = 'Notified {} attendees'.format(len(attendees))
+                
+        cur.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(status, new_completed_date, notification_id))
+        db.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        db.rollback()
+        
+    finally:
+        cur.close()
+        db.close()
         
 
